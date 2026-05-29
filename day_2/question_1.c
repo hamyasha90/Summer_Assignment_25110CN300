b36e6db5f50e0d867f824d06f76ababb4d6387d7
hamyasha90 <harshgoyal14725@gmail.com>
2026-05-29 16:41:58 +0530

    day 2 assignment

---
+++ day_2/question_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+   
+    for (; num != 0; num = num / 10) {
+        digit = num % 10;   
+        sum = sum + digit;  
+    }
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
